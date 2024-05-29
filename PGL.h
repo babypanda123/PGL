6c5b4e6c125546f17e3a7c248ced28edb2de1bc6
babypanda123 <brainweon@naver.com>
2024-05-29 22:11:45 +0900

    Update PGL.h

--- PGL.h
+++ PGL.h
@@ -1,33 +1,29 @@
-#ifndef PGL_h
-#define PGL_h
+#ifndef PGL_H
+#define PGL_H
 
-#include <Arduino.h>
-#include <HardwareSerial.h>
+#include "sdkconfig.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_spp_api.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include <cstring>
 
-class PGL : public Stream {
+class SimpleBluetoothSerial {
 public:
-    PGL();
-    ~PGL();
-
-    void begin(String name);
-    void end();
-
-    virtual int available();
-    virtual int read();
-    virtual int peek();
-    virtual size_t write(uint8_t data);
-    virtual void flush();
-
-    using Print::write;
-
-    operator bool() {
-        return true;
-    }
-
+    SimpleBluetoothSerial();
+    
+    bool begin(const char* deviceName);
+    size_t write(uint8_t c);
+    int available();
+    int read();
+    
 private:
-    HardwareSerial *serial;
+    static void spp_callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param);
+    
+    static std::string _spp_data;
+    static uint32_t _spp_client;
 };
 
-extern PGL PGL_BT;
-
-#endif
+#endif // PGL_H
