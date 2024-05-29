9bdeb18701013e2cba6854554c66c767743c4fc8
babypanda123 <brainweon@naver.com>
2024-05-29 22:12:19 +0900

    Update PGL.cpp

--- PGL.cpp
+++ PGL.cpp
@@ -1,46 +1,51 @@
 #include "PGL.h"
 
-PGL PGL_BT;
+std::string SimpleBluetoothSerial::_spp_data = "";
+uint32_t SimpleBluetoothSerial::_spp_client = 0;
 
-PGL::PGL() {
-    serial = &Serial; // 기본 시리얼 포트를 사용합니다. 필요에 따라 변경할 수 있습니다.
+SimpleBluetoothSerial::SimpleBluetoothSerial() : _spp_client(nullptr) {}
+
+bool SimpleBluetoothSerial::begin(const char* deviceName) {
+    if (!btStart()) {
+        return false;
+    }
+    
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
+    
+    esp_spp_register_callback(spp_callback);
+    esp_spp_init(ESP_SPP_MODE_CB);
+    
+    esp_bt_dev_set_device_name(deviceName);
+    esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+    
+    return true;
 }
 
-PGL::~PGL() {
-    // 아무 작업도 필요하지 않습니다.
+size_t SimpleBluetoothSerial::write(uint8_t c) {
+    if (_spp_client) {
+        esp_spp_write(_spp_client, 1, &c);
+    }
+    return 1;
 }
 
-void PGL::begin(String name) {
-    // Bluetooth 시리얼 통신을 시작합니다.
-    serial->begin(9600); // 기본 속도는 9600으로 설정됩니다. 필요에 따라 변경할 수 있습니다.
+int SimpleBluetoothSerial::available() {
+    return _spp_data.length();
 }
 
-void PGL::end() {
-    // Bluetooth 시리얼 통신을 종료합니다.
-    serial->end();
+int SimpleBluetoothSerial::read() {
+    if (_spp_data.length() > 0) {
+        char c = _spp_data[0];
+        _spp_data.erase(0, 1);
+        return c;
+    }
+    return -1;
 }
 
-int PGL::available() {
-    // 수신 버퍼에 있는 바이트 수를 반환합니다.
-    return serial->available();
-}
-
-int PGL::read() {
-    // 다음 바이트를 읽고 반환합니다.
-    return serial->read();
-}
-
-int PGL::peek() {
-    // 다음 읽을 바이트를 확인합니다.
-    return serial->peek();
-}
-
-size_t PGL::write(uint8_t data) {
-    // 데이터를 보냅니다.
-    return serial->write(data);
-}
-
-void PGL::flush() {
-    // 송신 버퍼를 비웁니다.
-    serial->flush();
+void SimpleBluetoothSerial::spp_callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+    if (event == ESP_SPP_DATA_IND_EVT) {
+        _spp_data.append((char*)param->data_ind.data, param->data_ind.len);
+    } else if (event == ESP_SPP_SRV_OPEN_EVT) {
+        _spp_client = param->srv_open.handle;
+    }
 }
